f4193d3e2af343e2d930dc09b846caac27f3a098
Natan Humieniuk <nhumieniuk@bsd220.org>
2022-02-02 10:21:30 -0600

    Initial Commit

---
+++ PickADriver/PickADriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickADriverApp.swift
+//  PickADriver
+//
+//  Created by Student on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PickADriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
